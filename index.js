b1a792fffb211f77c0ef79d5468c260f6296a1f2
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-06-08 14:13:41 +0530

    Task_8June_Fixed_Middlware

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 import express from 'express';
 import path from "path";
+import bodyParser from 'body-parser'; //middleware
+
 const __dirname = path.resolve("./");
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-//Route define
+// Custom middleware
+function validateUser(req, res, next) {
+    if (req.body.userid === 'abc' && req.body.pass === 'def') {
+        res.send("User login Success!");
+    } else {
+        res.status(401).send("Unauthorized");
+    }
+}
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
-
+// Route definitions
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     res.send("About Us");
-})
+});
 
-app.get('/user',(req,res)=>{
-    res.sendFile(__dirname+'/user.json')
-    console.log(__dirname+'/user.json');
-    
-})
+app.get('/user', (req, res) => {
+    res.sendFile(__dirname + '/user.json');
+    console.log(__dirname + '/user.json');
+});
 
+app.post('/submit', validateUser, (req, res) => {
+    // If validateUser middleware doesn't end the request, this will run
+    res.send("Form submitted");
+});
 
-//Server initialise
-app.listen(3000,()=>{
+// Server initialization
+app.listen(3000, () => {
     console.log(`Server is running on port 3000`);
 });
